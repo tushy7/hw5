036f2339dd61d9d4c81591cab67b00b4f2b4074b
Tushar Zhade <zhade@usc.edu>
2025-04-26 06:39:55 +0000

    added new files

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool fillSchedule(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, size_t day, vector<size_t>& shiftsWorked);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -37,9 +38,50 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    for(size_t i = 0; i < avail.size(); ++i){
+        sched.push_back(vector<Worker_T>());
+    }
 
-
-
-
+    vector<size_t> shiftsWorked(avail[0].size(), 0);
+    return fillSchedule(avail, dailyNeed, maxShifts, sched, 0, shiftsWorked);
 }
+
+// recursive helper function to fill the schedule
+bool fillSchedule(
+    const AvailabilityMatrix& avail,
+    size_t dailyNeed,
+    size_t maxShifts,
+    DailySchedule& sched,
+    size_t day,
+    vector<size_t>& shiftsWorked
+)
+{
+    if(day == avail.size()){
+        return true;
+    }
+
+    if(sched[day].size() == dailyNeed){
+        return fillSchedule(avail, dailyNeed, maxShifts, sched, day + 1, shiftsWorked);
+    }
+
+    for(size_t worker = 0; worker < avail[0].size(); ++worker){
+        if(avail[day][worker] && shiftsWorked[worker] < maxShifts && find(sched[day].begin(), sched[day].end(), worker) == sched[day].end()){
+            sched[day].push_back(worker);
+            ++shiftsWorked[worker];
+
+            if(fillSchedule(avail, dailyNeed, maxShifts, sched, day, shiftsWorked)){
+                return true;
+            }
+
+            // backtrack
+            sched[day].pop_back();
+            --shiftsWorked[worker];
+        }
+    }
+
+    return false;
+}
+
+
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -15,14 +15,60 @@ using namespace std;
 // Add prototypes of helper functions here
 
 
-// Definition of primary wordle function
-std::set<std::string> wordle(
-    const std::string& in,
-    const std::string& floating,
-    const std::set<std::string>& dict)
-{
-    // Add your code here
+void buildWords(string partial, const string& in, string floating, const set<string>& dict, set<string>& results, int index);
+bool matchesConstraints(const string& word, const string& in, string floating);
 
+// definition of primary wordle function
+set<string> wordle(
+    const string& in,
+    const string& floating,
+    const set<string>& dict)
+{
+    set<string> results;
+    buildWords("", in, floating, dict, results, 0);
+    return results;
 }
 
-// Define any helper functions here
+// recursive helper function to build all possible words
+void buildWords(string partial, const string& in, string floating, const set<string>& dict, set<string>& results, int index)
+{
+    // base case: finished building a full word
+    if(index == (int)in.size()){
+        if(dict.find(partial) != dict.end() && matchesConstraints(partial, in, floating)){
+            results.insert(partial);
+        }
+        return;
+    }
+
+    // fixed letter given
+    if(in[index] != '-'){
+        buildWords(partial + in[index], in, floating, dict, results, index + 1);
+    }
+    else{
+        // try all lowercase letters at this position
+        for(char c = 'a'; c <= 'z'; ++c){
+            buildWords(partial + c, in, floating, dict, results, index + 1);
+        }
+    }
+}
+
+// checks if a completed word satisfies floating letter requirements
+bool matchesConstraints(const string& word, const string& in, string floating)
+{
+    // make a copy of floating letters to cross off
+    for(int i = 0; i < (int)word.size(); ++i){
+        if(in[i] == '-'){
+            size_t pos = floating.find(word[i]);
+            if(pos != string::npos){
+                floating.erase(pos, 1);
+            }
+        }
+        else{
+            if(word[i] != in[i]){
+                return false;
+            }
+        }
+    }
+    // must have used up all floating letters
+    return floating.empty();
+}
